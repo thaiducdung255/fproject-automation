2211bb9e0da84c0fc830475d3fc0f1a1c61779c2
DungTD10 <mducdung255@gmail.com>
2022-01-05 22:32:42 +0700

    remove selenium in verify mode

--- main.js
+++ main.js
@@ -11,10 +11,6 @@ const {
 
 const { isFproject, isResoveMode } = require('./config');
 
-if (process.platform.slice(0, 3) === 'win') {
-   chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
-}
-
 let driver;
 
 process.on('uncaughtException', async (err) => {
@@ -28,19 +24,23 @@ process.on('SIGINT', async () => {
    process.exit(1);
 });
 
-const caps = Capabilities.chrome();
-const options = new chrome.Options(caps);
-options.addArguments('headless');
-
-driver = new Builder()
-   .forBrowser('chrome')
-   .setChromeOptions(options)
-   .build();
-
 async function init() {
    const page = isFproject ? 'https://fproject.fpt.vn' : 'https://redmine.org/login';
 
    if (isResoveMode) {
+      if (process.platform.slice(0, 3) === 'win') {
+         chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+      }
+
+      const caps = Capabilities.chrome();
+      const options = new chrome.Options(caps);
+      options.addArguments('headless');
+
+      driver = new Builder()
+         .forBrowser('chrome')
+         .setChromeOptions(options)
+         .build();
+
       console.log(`loading page: ${page}`);
       await driver.get(page);
 
